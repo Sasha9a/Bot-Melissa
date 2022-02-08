13f13af95a96c0ed22592c373befe05714f8855d
adovgiy <adovgiy@zaoblako.ru>
2022-02-08 11:38:56 +0300

    Проверка VK API

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  token: process.env.TOKEN || 'a0449c8f66f516685c91c5f0ae33d39cc803351feac74e39c9d31fda96efeb6fe5ee57a5d53805cae6dc8'
 };
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,10 +5,35 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { API, Updates, Upload } from "vk-io";
 
 import { AppModule } from './app/app.module';
+import { environment } from "./environments/environment";
 
 async function bootstrap() {
+  const api = new API({
+    token: environment.token
+  })
+  const upload = new Upload({
+    api
+  });
+  const updates = new Updates({
+    api,
+    upload
+  });
+  const users = await api.users.get({
+    user_ids: 'aleksandr_dovgiy'
+  });
+  const messages = await api.messages.send({
+    user_id: 283215047,
+    random_id: Math.random() * 2000000,
+    message: 'Проверка'
+  });
+  console.log(messages);
+  console.log(users);
+  updates.on('message_new', (context) => {
+    console.log(context);
+  });
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
