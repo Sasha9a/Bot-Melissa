aa21af775955d46d8ad356bf13280550e0e15ca7
Aleksandr <aleksandr_dovgiyy@mail.ru>
2022-12-29 01:28:18 +0300

    dotenv

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,5 +1,8 @@
 import { botInit } from '@bot-melissa/app/vk';
+import * as dotenv from 'dotenv';
 import * as mongoose from 'mongoose';
+
+dotenv.config();
 
 const main = () => {
   mongoose.set('strictQuery', true);
