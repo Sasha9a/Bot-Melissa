21fda685a836f33e52d713179b76c319a69fc18d
rivera-developer <daniilkarleole@gmail.com>
2023-10-27 16:50:48 +0400

    Попытка исправления OnlineList

--- apps/app/src/app/module/chat/chat.vk.ts
+++ apps/app/src/app/module/chat/chat.vk.ts
@@ -239,6 +239,7 @@ export const getChat = async (req: RequestMessageVkModel) => {
 export const onlineList = async (req: RequestMessageVkModel) => {
   if (req.msgObject.peerType == PeerTypeVkEnum.CHAT) {
     const membersList = req.members.filter((m) => m.profile && m.profile.online_info?.is_online);
+    console.log(membersList);
     let result = 'Список пользователей онлайн:';
     for (let i = 0; i != membersList.length; i++) {
       result = result.concat(
@@ -246,7 +247,6 @@ export const onlineList = async (req: RequestMessageVkModel) => {
           membersList[i].info?.icon ? ' ' + membersList[i].info?.icon : ''
         }`
       );
-      result = result.concat(` - (${membersList[i].profile.online_info?.is_mobile ? '📱' : '🖥'})`);
     }
     req.msgObject.send(result, { disable_mentions: true }).catch(console.error);
   }
