874fcd5777f7ec8f864f7691b48844e69b9c12c4
Aleksandr <aleksandr_dovgiyy@mail.ru>
2024-04-12 23:12:53 +0300

    Исправление времени инвайта пользователя.

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,4 +1,5 @@
 import { botInit } from '@bot-melissa/app/vk';
+import { Client } from '@elastic/elasticsearch';
 import * as dotenv from 'dotenv';
 import * as mongoose from 'mongoose';
 
@@ -7,6 +8,13 @@ dotenv.config();
 const main = () => {
   mongoose.set('strictQuery', true);
   botInit();
+  const client = new Client({
+    node: 'http://localhost:9200',
+    auth: {
+      username: 'elastic',
+      password: 'changeme'
+    }
+  });
 };
 
 main();
